7052b40dd2150ac8c21829151e0ea3351cb90527
JerkyZebra <arthurmesquita06@gmail.com>
2025-02-21 12:17:08 -0300

    Mexendo com a NavBar

--- pw3-somativa-dos-maloka/src/App.jsx
+++ pw3-somativa-dos-maloka/src/App.jsx
@@ -1,42 +1,13 @@
 import './App.css'
 
-import BookCard from './components/BookCard'
-
-import capa_livro from './assets/cavernas_aco.jpg'
+import NavBar from './components/layout/NavBar' 
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>PW3 - WEBAPP - LIVRARIA</h1>
-        <BookCard
-        titulo='Quarto de despejo'
-        autor='Carolina Maria de Jesus'
-        imagem={capa_livro}/>
-
-        
-        {/* <BookCard 
-        titulo= 'Dom Casmurro'
-        autor= 'Machado de Assis'
-        imagem= 'Uma imagem deve aparecer aqui'/>
-
-        <BookCard
-        titulo= 'Memórias Póstumas de Bras Cubas'
-        autor= 'Machado de Assis'
-        imagem= 'Uma imagem deve aparecer aqui'/>
-
-        <BookCard
-        titulo='Iracema'
-        autor='José de Alencar'
-        imagem='Uma imagem deve aparecer aqui'/>
-
-
-        <BookCard
-        titulo='Til'
-        autor='José de Alencar'
-        imagem='Uma imagem deve aparecer aqui'/> */}
-
+        <NavBar />
       </div>
     
     </>
