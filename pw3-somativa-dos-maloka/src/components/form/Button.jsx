1aa6ab430daa39a9898038cbc0dc479264fa7a72
JerkyZebra <arthurmesquita06@gmail.com>
2025-06-04 20:23:41 -0300

    Atualizando o projeto. A essa altura o projeto já permite a navegação pelo site, inclusão e listagem de livros, barra de navegação, além da integração do REACT com a API REST

--- pw3-somativa-dos-maloka/src/components/form/Button.jsx
+++ pw3-somativa-dos-maloka/src/components/form/Button.jsx
@@ -2,11 +2,10 @@ import style from './Button.module.css'
 
 const Button = ({label})=>{
     return(
-        
-    <div className = {buttonContainer}>
-        <button>{label}</button>
-    </div>
-    )
+            <div>
+                <button>{label}</button>
+            </div>
+    )  
 }
 
 export default Button
